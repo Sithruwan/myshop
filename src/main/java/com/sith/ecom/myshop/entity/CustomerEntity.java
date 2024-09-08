500a1c787ae3ca354057a79e5f02f0464f059852
sithruwan <madhujithasithruwan@gmail.com>
2024-09-08 23:36:00 +0530

    Customer entity done

---
+++ src/main/java/com/sith/ecom/myshop/entity/CustomerEntity.java
@@ -0,0 +1,35 @@
+package com.sith.ecom.myshop.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "customer")
+public class CustomerEntity {
+    @Id
+    @Column(name = "property_id",nullable = false,length = 80)
+    private String propertyId;
+
+    @Column(name = "name",nullable = false,length = 80)
+    private String name;
+
+    @Column(name = "email",nullable = false,length = 100,unique = true)
+    private String email;
+
+    @Column(name = "phone",nullable = false,length = 20)
+    private String phone;
+
+    @Column(name = "address",nullable = false,length = 255)
+    private String address;
+
+    @Column(name = "is_active",columnDefinition = "TINYINT")
+    private boolean isActive;
+}
